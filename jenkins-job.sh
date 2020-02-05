53be5ed41b03bce9214e5b9972b4b29d66f928aa
Khem Raj <raj.khem@gmail.com>
2020-02-04 18:16:22 -0800

    jenkins-job.sh: Fix formatting for BB_DISKMON_DIRS
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -310,14 +310,14 @@ INHERIT += "reproducible_build_simple"
 
 XZ_DEFAULTS = "--threads=8"
 
-BB_DISKMON_DIRS = "\
-    STOPTASKS,${TMPDIR},1G,100K \\\
-    STOPTASKS,${DL_DIR},1G,100K \\\
-    STOPTASKS,${SSTATE_DIR},1G,100K \\\
-    STOPTASKS,/tmp,100M,100K \\\
-    ABORT,${TMPDIR},100M,1K \\\
-    ABORT,${DL_DIR},100M,1K \\\
-    ABORT,${SSTATE_DIR},100M,1K \\\
+BB_DISKMON_DIRS = "\\
+    STOPTASKS,${TMPDIR},1G,100K \\
+    STOPTASKS,${DL_DIR},1G,100K \\
+    STOPTASKS,${SSTATE_DIR},1G,100K \\
+    STOPTASKS,/tmp,100M,100K \\
+    ABORT,${TMPDIR},100M,1K \\
+    ABORT,${DL_DIR},100M,1K \\
+    ABORT,${SSTATE_DIR},100M,1K \\
     ABORT,/tmp,10M,1K"
 
 #require world_fixes.inc
