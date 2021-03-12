d4d80883454aab98710cbf1fc1c8e435669746b7
Khem Raj <raj.khem@gmail.com>
2021-03-12 08:39:05 -0800

    jenkins-jobs: Pin BB_NUMBER_THREADS to half of number of CPUs
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -334,6 +334,9 @@ TMPDIR = "${TMPFS}"
 DL_DIR = "${DOWNLOADS}"
 SSTATE_DIR = "${SSTATE}"
 
+#PARALLEL_MAKE_append = " -l \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/100)}"
+BB_NUMBER_THREADS = "\${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/200)}"
+#PARALLEL_MAKE = "-j \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/200)}"
 XZ_DEFAULTS = "--threads=8"
 
 INHERIT += "rm_work"
