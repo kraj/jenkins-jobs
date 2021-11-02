5d04352dc01b3122497cd0a85c98b4bcd963c832
Khem Raj <raj.khem@gmail.com>
2021-11-01 17:44:36 -0700

    jenkins-job.sh: Use https protocol instead of git
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -325,7 +325,7 @@ function run_prepare {
       mv ${BUILD_TOPDIR}/../buildhistory .
     fi
     git init
-    git remote add origin git://github.com/YoeDistro/yoe-distro
+    git remote add origin https://github.com/YoeDistro/yoe-distro
     git pull origin ${BUILD_BRANCH}
     git checkout origin/${BUILD_BRANCH}
     cat <<EOF > ${BUILD_TOPDIR}/conf/local.conf
