2e3638eb8c448346b2b0c65209124e51035fd0c1
Khem Raj <raj.khem@gmail.com>
2023-08-11 09:04:31 -0700

    jenkins-job.sh: Append -l option to PARALLEL_MAKE
    Manages load on system based on the NCPUs
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -139,9 +139,9 @@ DISTRO_FEATURES:append = " ptest"
 # Allow 3 mins to let it boot
 TEST_QEMUBOOT_TIMEOUT = "60"
 
-#PARALLEL_MAKE:append = " -l \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/100)}"
-BB_NUMBER_THREADS = "\${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
-PARALLEL_MAKE = "-j \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
+PARALLEL_MAKE:append = " -l \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/100)}"
+#BB_NUMBER_THREADS = "\${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
+#PARALLEL_MAKE = "-j \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
 
 XZ_DEFAULTS = "--threads=8"
 ZSTD_THREADS = "8"
