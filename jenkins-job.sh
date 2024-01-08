b688eff4245f0bc6a282022b761262c548c05df1
Khem Raj <raj.khem@gmail.com>
2024-01-07 20:13:12 -0800

    jenkins-job.sh: Increase BB_PRESSURE_MAX_CPU to 32000
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -145,7 +145,7 @@ TEST_QEMUBOOT_TIMEOUT = "60"
 #PARALLEL_MAKE = "-j \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
 #PARALLEL_MAKE:append = " -l \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/50)}"
 
-BB_PRESSURE_MAX_CPU = "16000"
+BB_PRESSURE_MAX_CPU = "32000"
 #BB_PRESSURE_MAX_MEMORY = "20000"
 
 #XZ_THREADS = "4"
