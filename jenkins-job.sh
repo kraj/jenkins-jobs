1f812195e1dc5e2469f2fe7c51f61d00d342141b
Khem Raj <raj.khem@gmail.com>
2024-01-10 23:23:39 -0800

    jenkins-job.sh: Do not use buildtools tarball on ubu 22.04
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -23,8 +23,8 @@ version_greater_equal() {
 test -e /etc/os-release && os_release='/etc/os-release' || os_release='/usr/lib/os-release'
 . "${os_release}"
 
-# use Pre-Built buildtools Tarball ( currently 4.1.2 is latest, update it on ubuntu 18.04 hosts when next release happens)
-if ! version_greater_equal $VERSION_ID 24.04; then
+# use Pre-Built buildtools Tarball ( currently 4.3.1 is latest, update it on ubuntu 18.04 hosts when next release happens)
+if ! version_greater_equal $VERSION_ID 22.04; then
         BUILDTOOLS=/opt/poky/4.3.1/environment-setup-x86_64-pokysdk-linux
         echo "Using buildtools from $BUILDTOOLS"
         test -e ${BUILDTOOLS} && . ${BUILDTOOLS}
