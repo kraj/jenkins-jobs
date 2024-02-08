601bf284c2bd82ecd8cc3ec3eddbfbfff8d15576
Khem Raj <raj.khem@gmail.com>
2024-02-08 11:24:50 -0800

    jenkins-job.sh: Set BB_NUMBER_THREADS/PARALLEL_MAKE
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -143,13 +143,13 @@ DISTRO_FEATURES:append = " ptest"
 # Allow 3 mins to let it boot
 TEST_QEMUBOOT_TIMEOUT = "60"
 
-#BB_NUMBER_THREADS = "\${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
-#PARALLEL_MAKE = "-j \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
-#PARALLEL_MAKE:append = " -l \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/50)}"
+BB_NUMBER_THREADS = "\${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
+PARALLEL_MAKE = "-j \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/300)}"
+PARALLEL_MAKE:append = " -l \${@int(os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN']) * 100/50)}"
 
-#BB_NUMBER_PARSE_THREADS = "32"
 BB_PRESSURE_MAX_CPU = "100000"
 #BB_PRESSURE_MAX_MEMORY = "20000"
+#BB_NUMBER_PARSE_THREADS = "32"
 
 #XZ_THREADS = "4"
 #ZSTD_THREADS = "4"
